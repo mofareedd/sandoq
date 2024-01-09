bbffa75edf92830576e20c3c3988d2573d063122
mofareed-97 <m.fareeed1997@gmail.com>
2024-01-09 03:03:27 +0300

    feat: products horizontal & vertical logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import Categories from "@/components/categories";
-import { ProductCard } from "@/components/product-card";
+import { ProductCard, ProductsList } from "@/components/product";
 import { useProducts } from "@/hooks/useProducts";
 import { Link } from "expo-router";
 import { ShoppingBag } from "lucide-react-native";
@@ -11,40 +11,35 @@ export default function Home() {
   const { data } = useProducts();
   return (
     <SafeAreaView className="flex-1 py-4 gap-4">
-      <View className="flex-row items-center justify-between px-6">
-        <Text className="text-3xl font-medium">Sandoq</Text>
-        <ShoppingBag color={"#333"} />
-      </View>
-
-      <View className="my-4">
-        <View className="flex-row items-center justify-between px-6 mb-4">
-          <Text className="text-xl font-medium">Categories</Text>
-          <Link href={""}>
-            <Text className="text-muted-foreground text-lg">See All</Text>
-          </Link>
+      <ScrollView className="flex-1 py-4 gap-4">
+        <View className="flex-row items-center justify-between px-6">
+          <Text className="text-3xl font-medium">Sandoq</Text>
+          <View className="relative">
+            <ShoppingBag color={"#333"} />
+            <View className="absolute bg-red-500 z-10 w-5 h-5 rounded-full -top-2 -right-2 items-center justify-center">
+              <Text className="text-white text-xs">6</Text>
+            </View>
+          </View>
         </View>
-        <Categories />
-      </View>
 
-      <View className="">
-        <Text className="px-6 font-bold text-xl">Flash Sale</Text>
-        <ScrollView
-          horizontal
-          className=""
-          showsHorizontalScrollIndicator={false}
-        >
-          {data && data.length
-            ? data.map((product) => (
-                <View
-                  className="items-center ml-6 gap-2 relative"
-                  key={product.id}
-                >
-                  <ProductCard product={product} />
-                </View>
-              ))
-            : null}
-        </ScrollView>
-      </View>
+        <View className="my-4">
+          <View className="flex-row items-center justify-between px-6 mb-4">
+            <Text className="text-xl font-medium">Categories</Text>
+            <Link href={""}>
+              <Text className="text-muted-foreground text-lg">See All</Text>
+            </Link>
+          </View>
+          <Categories />
+        </View>
+
+        {data ? (
+          <ProductsList
+            products={data}
+            label="Flash Sale"
+            isHorizontal={false}
+          />
+        ) : null}
+      </ScrollView>
     </SafeAreaView>
   );
 }
---
+++ components/product.tsx
@@ -0,0 +1,122 @@
+import { ProductType } from "@/types";
+import React from "react";
+import { Pressable, ScrollView, Text, View } from "react-native";
+import ProductImage from "./product-image";
+import { BASE_URL } from "@/lib/constants";
+import { Heart, ShoppingBag } from "lucide-react-native";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+interface ProductListProps {
+  products: ProductType[];
+  label: string;
+  isHorizontal?: boolean;
+}
+export function ProductsList({
+  products,
+  label,
+  isHorizontal = false,
+}: ProductListProps) {
+  return (
+    <View className="">
+      <Text className="px-6 mb-4 font-bold text-xl">{label}</Text>
+      <ScrollView
+        horizontal={isHorizontal}
+        className=""
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      >
+        {products.map((product) => (
+          <View
+            className={cn(
+              "gap-2 relative",
+              isHorizontal ? "items-center ml-6  my-0" : "items-start px-6 my-2"
+            )}
+            key={product.id}
+          >
+            <ProductCard product={product} isRow={!isHorizontal} />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+export function ProductCard({
+  product: { attributes },
+  isRow = false,
+}: {
+  product: ProductType;
+  isRow?: boolean;
+}) {
+  return (
+    <View className={cn("gap-4", isRow ? "flex-row" : "flex-col")}>
+      <View
+        className={cn(
+          "rounded-lg border border-border overflow-hidden relative",
+          isRow ? "w-32 h-40" : "w-40 h-52"
+        )}
+      >
+        <View className="absolute p-4  z-10 flex-row items-center justify-between w-full">
+          <Text className="text-background bg-orange-400 rounded-md overflow-hidden px-2 py-1 text-xs font-bold">
+            50%
+          </Text>
+          <Pressable>
+            <Heart size={20} className="text-muted-foreground" />
+          </Pressable>
+        </View>
+        <ProductImage
+          source={`${BASE_URL}${attributes.images.data[0]?.attributes.url}`}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+          }}
+        />
+      </View>
+      <View className={cn("py-2", isRow ? "flex-1" : "")}>
+        <Text
+          className={cn(
+            isRow ? "max-w-60 line-clamp-3" : "max-w-40 line-clamp-2"
+          )}
+        >
+          {attributes.name}
+        </Text>
+        <View
+          className={cn(
+            "flex-row justify-between items-end py-2",
+            isRow ? "flex-1" : ""
+          )}
+        >
+          <View
+            className={cn("gap-2", isRow ? "flex-col-reverse" : "flex-row")}
+          >
+            <Text
+              className={cn("", isRow ? "text-xl font-bold" : "font-medium")}
+            >
+              ${attributes.price.toFixed(2)}
+            </Text>
+            <Text className="text-muted-foreground text-xs line-through">
+              ${attributes.price.toFixed(2)}
+            </Text>
+          </View>
+          {isRow ? (
+            <Button
+              size={"sm"}
+              className="text-xs w-20 h-10 flex-row items-center gap-2"
+              textClass="text-white"
+            >
+              <ShoppingBag size={16} className="text-background" />
+              <Text className="text-background">Buy</Text>
+            </Button>
+          ) : null}
+          {/* {!isRow ? (
+            <Text className="text-muted-foreground">
+              {attributes.stock} left
+            </Text>
+          ) : null} */}
+        </View>
+      </View>
+    </View>
+  );
+}
